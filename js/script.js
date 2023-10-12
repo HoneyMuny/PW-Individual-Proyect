8d71331802f53d39b0a1151186b941e13af5112e
Diana AM <71198689+HoneyMuny@users.noreply.github.com>
2023-10-11 22:12:13 -0600

    Update #2
    More stuff added

--- js/script.js
+++ js/script.js
@@ -6,17 +6,52 @@ window.onload=function(){
     btn1=document.getElementById("btn1");
     btn2=document.getElementById("btn2");
     btn3=document.getElementById("btn3");
+    btn4=document.getElementById("menu");
 
-    btn1.onmouseover=function(){
-        btn1.style.color="orange";
+    function btnOnMouseOver(btn){
+        btn.style.color="orange";
     }
-    btn1.onmousedown=function(){
-        btn1.style.color="blue";
+    function btnOnMouseDown (btn){
+        btn.style.color="blue";
     }
-    btn1.onmouseup=function(){
-        btn1.style.color="#09F"
+    function btnOnMouseUp(btn){
+        btn.style.color="#09F"
     }
-    btn1.onmouseout=function(){
-        btn1.style.color="pink"
+    function btnOnMouseOut(btn){
+        btn.style.color="pink"
     }
+
+    btn1.addEventListener("mouseOver", function(){btnOnMouseOver(btn1);});
+    btn1.addEventListener("mouseDown", function(){btnOnMouseDown(btn1);});
+    btn1.addEventListener("mouseUp", function(){btnOnMouseUp(btn1);});
+    btn1.addEventListener("mouseOut", function(){btnOnMouseOut(btn1);});
+
+    btn2.addEventListener("mouseOver", function(){btnOnMouseOver(btn2);});
+    btn2.addEventListener("mouseDown", function(){btnOnMouseDown(btn2);});
+    btn2.addEventListener("mouseUp", function(){btnOnMouseUp(btn2);});
+    btn2.addEventListener("mouseOut", function(){btnOnMouseOut(btn2);});
+
+    btn3.addEventListener("mouseOver", function(){btnOnMouseOver(btn3);});
+    btn3.addEventListener("mouseDown", function(){btnOnMouseDown(btn3);});
+    btn3.addEventListener("mouseUp", function(){btnOnMouseUp(btn3);});
+    btn3.addEventListener("mouseOut", function(){btnOnMouseOut(btn3);});
+
+    btn4.addEventListener("mouseOver", function(){btnOnMouseOver(btn4);});
+    btn4.addEventListener("mouseDown", function(){btnOnMouseDown(btn4);});
+    btn4.addEventListener("mouseUp", function(){btnOnMouseUp(btn4);});
+    btn4.addEventListener("mouseOut", function(){btnOnMouseOut(btn4);});
+
+    let osc=document.getElementById("oscuro");
+    btn1.addEventListener("click", shSearch);
+    osc.addEventListener("click", shSearch);
+
+    function shSearch(){
+        if (osc.style.display=="" || osc.style.display=="none") {
+            osc.style.display="inline-block";
+        } else {
+            osc.style.display="none";
+        }
+    }
+
+
 }
